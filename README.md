90bd6a0bbeb310cea4e53984be88c211bc207205
Ana Suput <ana.suput@sva.de>
2022-09-18 23:14:09 +0200

    Script to install required packages added

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # EfficientFrontier
+Run EfficientFrontier.Rmd to get a report which portfolio is the best, given the shares you choose. 
+It is recommended to run RequiredPackages.R first to be sure you installed necessary packages to run EfficientFrontier.Rmd. 
---
+++ RequiredPackages.R
@@ -0,0 +1,10 @@
+#Install required packages prior to install skript
+install.packages("tinytex")
+tinytex::install_tinytex()
+source("http://rnbeads.org/data/install.R")
+install.packages("tidyquant")
+install.packages("plotly")
+install.packages("timetk")
+install.packages("tidyr")
+install.packages("dplyr")
+install.packages("quantmod") # Bugfix for old yahoo finance URL from package "tidyquant"
